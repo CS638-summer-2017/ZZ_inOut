31c624255401280ffc1d6dd406f68d486d5cb7bf
Lijie Zhao <zhao@Lijies-MacBook-Pro.local>
2017-07-27 13:46:47 -0500

    Initial commit and adding the git ignore file.

--- inOut/ViewController.swift
+++ inOut/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBOutlet weak var outputView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("In buttonTapped \(inputField.text!)")
+//        print("In buttonTapped 2 \(inputField.text ?? "Default")")
+//        outputView.text = inputField.text!
+        outputView.text = "\(outputView.text!)\n\(inputField.text!)"
+        inputField.text = ""
+    }
+    
 }
 
